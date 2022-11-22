d3dbbd8f01ab6a6da5a17db88851078de21b2eb2
Çağdaş SANCARBARLAZ <cagdas@dincerteknoloji.com.tr>
2022-11-22 13:40:48 +0300

    Eticaret Sınıfları Oluşturuldu Hess.Core.xxx

---
+++ Yaznet_ESAT_Project/Hess.Core.EBSvc/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hess.Core.EBSvc
+{
+    public class Class1
+    {
+    }
+}
